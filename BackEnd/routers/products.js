e11303f16c5579c27d71eaf217b93ea667f2f764
Ashutosh <theashutosh16@gmail.com>
2022-06-26 21:59:25 +0530

    product API completed

--- BackEnd/routers/products.js
+++ BackEnd/routers/products.js
@@ -4,10 +4,17 @@ const router = express.Router();
 // const Product = require('../models/product'); // when product model is exported as -> module.exports = Product 
 const {Product} = require('../models/product');
 
+const mongoose = require('mongoose');
+
 /* get all products */
 router.get(`/`,async (req, res)=>{
     //   const productList = await Product.find().select('name image -_id')    -> get all product's name and image without id
-    const productList = await Product.find()
+    /* qurey parameter is when we give in /product?categories=1231231,123123 */
+    let filter = {}
+    if (req.query.category){
+        filter = {category: req.query.categories.split(',')}  
+    } // when categories is there -> It will filter otherwise will give all products
+    const productList = await Product.find(filter)
     if (!productList){
         res.status(500).json({success: false});
     }
@@ -66,6 +73,9 @@ router.post(`/`, async (req, res)=>{
 /* product edit/put */
 
 router.put('/:id', async (req, res)=>{
+    if (!mongoose.isValidObjectId(req.params.id)){
+        return res.status(400).json({ status: false, message: 'Invalid Product Id'})
+    }
     const category = await Category.findById(req.body.category)
     if (!category) return res.status(400).json({status: false, message:'Invalid Category'});
     const product = await Product.findByIdAndUpdate(req.params.id, {
@@ -84,7 +94,7 @@ router.put('/:id', async (req, res)=>{
         new: true
     });
     if (!product){
-        res.status(500).json({success: false, message: 'product id not found'})
+        return res.status(500).json({success: false, message: 'product id not found'})
     }
   //  res.status(200).send(product);
     res.send(product);
@@ -100,6 +110,25 @@ router.delete('/:id', (req, res)=>{
     }).catch((err)=>{
         return res.status(400).json({ success: false, error:err});
     })
+})
+
+/* get products count API */
+router.get(`/get/count`,async (req, res)=>{
+   const productCount = await Product.countDocuments()
+   if (!productCount){
+       return res.status(500).json({success: false});
+   }
+    res.send({productCount: productCount});
+})
+
+/* get featured products  */
+router.get(`/get/featured/:count`,async (req, res)=>{
+    const count = req.params.count ? req.params.count :  0;
+    const products = await Product.find({isFeatured: true}).limit(+count);
+   if (!products){
+       return res.status(500).json({success: false});
+   }
+    res.send({products: products});
 })
 
 module.exports=  router;    
